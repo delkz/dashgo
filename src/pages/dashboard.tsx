dc35507ad3b59f7f5b6f5b45c8e7443be19e5c89
David Silva <david.silva@seriedesign.com.br>
2022-07-29 18:05:39 -0300

    Component : Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Header } from '../components/Header';
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { Header } from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 // import { Container } from './styles';
 
 const pages: React.FC = () => {
-  return (<Header></Header>);
-}
+  return (
+    <Flex direction="column" h="100vh">
+      <Header></Header>
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar/>
+      </Flex>
+
+    </Flex>
+  );
+};
 
 export default pages;
