308306a578f22d68331e49d616248b6c512fe5b7
David Silva <davidlsilva.sp@gmail.com>
2022-08-23 08:36:52 -0300

    Mirage JS

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,42 @@
+import {createServer,Factory,Model} from 'miragejs';
+import { faker } from '@faker-js/faker'
+type User = {
+    user: string;
+    email: string;
+    created_at: string;
+}
+
+export function makeServer(){
+    const server = createServer({
+        models:{
+            user: Model.extend<Partial<User>>({})
+        },
+        factories:{
+            user: Factory.extend({
+                name(i:number){
+                    return `${faker.name.firstName()} ${faker.name.lastName()}`; 
+                },
+                email(){
+                    return faker.internet.email().toLowerCase();
+                },
+                createdAt(){
+                    return faker.date.recent(10).toISOString();
+                },
+            })
+        },
+        seeds(server){
+            server.createList('user',200)
+        },
+        routes(){
+            this.namespace = 'api';
+            this.timing = 750;
+            this.get('/users');
+            this.post('/users');
+            this.namespace = '';
+            this.passthrough();
+        }
+    });
+
+    return server;
+
+}
